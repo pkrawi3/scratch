e18c29f26e42c48e8e0b6ef92a8d932d55918127
Peter Krawiec <pkrawi3@gmail.com>
2024-08-05 08:15:21 -0500

    Basic table example added, hook up back end next?

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatListModule} from '@angular/material/list';
+import { TableComponent } from "./table/table.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatListModule],
+  imports: [RouterOutlet, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
