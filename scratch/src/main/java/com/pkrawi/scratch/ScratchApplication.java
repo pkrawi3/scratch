6196b9ba4568d4eb15a50af7f3afd6c13f29dfdd
Peter Krawiec <pkrawi3@gmail.com>
2024-08-07 22:24:53 -0500

    Initial spring app code

---
+++ scratch/src/main/java/com/pkrawi/scratch/ScratchApplication.java
@@ -0,0 +1,13 @@
+package com.pkrawi.scratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScratchApplication.class, args);
+	}
+
+}
---
+++ scratch/src/test/java/com/pkrawi/scratch/ScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkrawi.scratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
